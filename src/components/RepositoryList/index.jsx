d7ca71b9e1700966ec2029a463bfe17de0734d43
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-15 22:20:56 -0300

    feat: first React component

---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,29 @@
+
+const RepositoryName = 'Omni';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+          <a href="#">Access repository</a>
+        </li>
+        <li>
+          <strong>{RepositoryName}</strong>
+          <p>Forms in ReactJS</p>
+          <a href="#">Access repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+          <a href="#">Access repository</a>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+
