82453627bfcccf223b2cd87e28b077bf9e1439d2
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-15 22:53:42 -0300

    feat: state of the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import { RepositoryList } from './components/RepositoryList';
-import './styles/global.scss';
+import { Counter } from "./components/Counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
 export function App() {
   return (
-    <RepositoryList />
-  )
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
